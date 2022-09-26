b632dda7b14249721cbf49a45fe9d4884a31986c
jmg <jmg41490400@gmail.com>
2022-09-26 12:15:11 +0900

    직접 db 연결 및 crud 작업 구현

---
+++ jdbc-pratice/src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    // 테스트 코드를 실행하기 앞서 실행할 코드
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator,ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+        userDao.create(new User("wizard", "password", "name","email"));
+
+        User user = userDao.findByUserId("wizard");
+        assertThat(user).isEqualTo(new User("wizard", "password", "name","email"));
+      }
+}
